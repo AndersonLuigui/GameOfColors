f2012cb6fab92855b627c442e4e06b7a4d9e32d8
Anderson Luigui <alns1536@hotmail.com>
2023-05-26 13:20:04 -0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let inputr = document.getElementById("input-user");
+
+resposta.addEventListener('click', function(e) {
+    if (inputr.value === "Roxo" || inputr.value === "roxo") {
+        document.getElementById("mensage").innerHTML = ("Parabéns! Você acertou.");
+        document.getElementById("resposta-div").style.backgroundColor = ("purple");
+        document.getElementById("resposta-div").innerHTML = ("<h1>:)</h1>");
+    } else {
+        document.getElementById("mensage").innerHTML = ("Você errou! Tente novamente.");
+        document.getElementById("resposta-div").innerHTML = ("<h1>?</h1>");
+    e.preventDefault();
+    }
+
+});
+
+
+
+
+
+
+
+
+

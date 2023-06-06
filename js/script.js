5c4c7770086d8723ce57fafecc7dcc1bfc42b27b
Anderson Luigui <alns1536@hotmail.com>
2023-06-06 12:34:05 -0300

    Commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-let inputr = document.getElementById("input-user");
+let inputR = document.querySelector('#input-user');
+let faseDois = document.querySelector('#next');
 
-resposta.addEventListener('click', function(e) {
-    if (inputr.value === "Roxo" || inputr.value === "roxo") {
+inputR.addEventListener('keypress', function(e){
+    if(e.which == 13){
+        verificaResultado();
+      }
+    }, false);
+
+function verificaResultado() {
+    if (inputR.value === "Roxo" || inputR.value === "roxo") {
         document.getElementById("mensage").innerHTML = ("Parabéns! Você acertou.");
         document.getElementById("resposta-div").style.backgroundColor = ("purple");
         document.getElementById("resposta-div").innerHTML = ("<h1>:)</h1>");
+        buttonFase2();
     } else {
-        document.getElementById("mensage").innerHTML = ("Você errou! Tente novamente.");
-        document.getElementById("resposta-div").innerHTML = ("<h1>?</h1>");
-    e.preventDefault();
+        document.getElementById("mensage").innerHTML = ("Não foi dessa vez :/");
+    
     }
+}
 
-});
+function buttonFase2(){
+    document.querySelector('.btn').innerHTML = ('<button id="next"><a href="/pagina2.html">></a></button>');
+}
 
-
+function voltaAoNormal(){
+    document.getElementById("resposta-div").innerHTML = ("<h1>?</h1>");
+    document.getElementById("resposta-div").style.backgroundColor = ('');
+    document.getElementById("mensage").innerHTML = ('');
+    inputR.value = ('');
+}
 
 
 

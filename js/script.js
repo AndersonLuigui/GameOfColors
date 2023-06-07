8fa37f111d8216045a7d571e32414c4988b93a23
Anderson Luigui <alns1536@hotmail.com>
2023-06-07 14:38:05 -0300

    segunda fase

--- js/script.js
+++ js/script.js
@@ -1,27 +1,52 @@
-let inputR = document.querySelector('#input-user');
-let faseDois = document.querySelector('#next');
+const inputUser = document.querySelector('#input-user');
+const inputUser2 = document.querySelector('#input-user2');
 
-inputR.addEventListener('keypress', function(e){
-    if(e.which == 13){
-        verificaResultado();
-      }
-    }, false);
 
-function verificaResultado() {
-    if (inputR.value === "Roxo" || inputR.value === "roxo") {
-        document.getElementById("mensage").innerHTML = ("Parabéns! Você acertou.");
-        document.getElementById("resposta-div").style.backgroundColor = ("purple");
-        document.getElementById("resposta-div").innerHTML = ("<h1>:)</h1>");
-        buttonFase2();
-    } else {
-        document.getElementById("mensage").innerHTML = ("Não foi dessa vez :/");
-    
+/* inputUser.addEventListener('keypress', e => {
+    if(e.keyCode === 13){
+        faseUm();
+        e.preventDefault();
+        inputUser.removeEventListener('click', e);
     }
-}
+
+}); */
+
+
+
+inputUser2.addEventListener('keypress', e => {
+    if(e.keyCode === 13) {
+        faseDois();
+    }
+})
 
 function buttonFase2(){
     document.querySelector('.btn').innerHTML = ('<button id="next"><a href="/pagina2.html">></a></button>');
 }
+
+
+function faseUm() {
+    if(inputUser.value === 'roxo' || inputUser.value === 'Roxo'){
+        document.getElementById("mensage").innerHTML = ("Parabéns! Você acertou.");
+        document.getElementById("resposta-div").style.backgroundColor = ("purple");
+        document.getElementById("resposta-div").innerHTML = ("<h1>:)</h1>");
+        buttonFase2();
+
+    } else {
+        document.getElementById("mensage").innerHTML = ("Não foi dessa vez :/");
+    } 
+}
+
+
+function faseDois() {
+    if(inputUser2.value === 'laranja' || inputUser2.value === 'Laranja'){
+        document.getElementById("mensage").innerHTML = ("Parabéns! Você acertou.");
+        document.getElementById("resposta-div").style.backgroundColor = ("orange");
+        document.getElementById("resposta-div").innerHTML = ("<h1>:)</h1>");
+    } else {
+        document.getElementById("mensage").innerHTML = ("Não foi dessa vez :/");
+    } 
+}
+
 
 function voltaAoNormal(){
     document.getElementById("resposta-div").innerHTML = ("<h1>?</h1>");
@@ -29,10 +54,4 @@ function voltaAoNormal(){
     document.getElementById("mensage").innerHTML = ('');
     inputR.value = ('');
 }
-
-
-
-
-
-
 
